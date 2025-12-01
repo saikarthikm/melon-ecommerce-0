34963200d0e89d6fcee1b7b6640a15f69ceab3d0
saikarthikm <saikarthikm94@gmail.com>
2025-12-01 18:34:47 +0530

    template

---
+++ script.js
@@ -0,0 +1,11 @@
+fetch("products.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const list = document.getElementById("product-list");
+    data.forEach((item) => {
+      const div = document.createElement("div");
+      div.className = "product";
+      div.innerHTML = `<h3>item.name</h3><p>₹{item.price}</p><button>Add to Cart</button>`;
+      list.appendChild(div);
+    });
+  });
